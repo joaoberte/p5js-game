15db9b5fd8f2ccd27eda7322f8c8baa18c15f3c5
joaoberte <joaoberte2006@yahoo.com.br>
2020-06-24 07:52:38 -0300

    Assets added

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+var canvas2;
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(1400, 900);
+    canvas2 = createGraphics(1400, 900);
+    canvas2.clear();
+    background(map(mouseY, 0, 900, 0, 255), map(mouseX, 0, 1400, 0, 255), 100);
 }
 
 var greenSquare = {
@@ -8,14 +13,24 @@ var greenSquare = {
 }
 
 function draw() {
-    background(map(mouseY, 0, 600, 0, 255), map(mouseX, 0, 800, 0, 255), 100);
+    background(map(mouseY, 0, 900, 0, 255), map(mouseX, 0, 1400, 0, 255), 100);
 
+    //Canvas2 Objects
+    if (mouseIsPressed) {
+        canvas2.fill(255, 150);
+        canvas2.noStroke();
+        canvas2.ellipse(mouseX, mouseY, 50, 50);
+    }
+
+    image(canvas2, 0 , 0);
+
+    //Canvas1 Objects
     stroke(200, 200, 200);
     strokeWeight(6);
 
     fill(200, 250, 200);
 
-    if (greenSquare.x > 700)
+    if (greenSquare.x > 1300)
         greenSquare.dir = -5;
     else if (greenSquare.x < 0)
         greenSquare.dir = 5;
@@ -34,7 +49,3 @@ function draw() {
     fill(250, 200, 200, 150);
     rect(400, 200, 150, 200);
 }
-
-  //function mousePressed() {
-  //  background(100, 100, 150);
-  //}
